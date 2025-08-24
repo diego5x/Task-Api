c35bba0e998c6f5e18a413c54ebcdf456295189e
Diego <diego444x@gmai.com>
2025-08-24 10:11:59 -0300

    gitignore created

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 source 'https://rubygems.org'
 
+gem 'erb'
 gem 'sinatra'
 gem 'sequel'
 gem 'sqlite3'
 gem 'json'
 gem 'sinatra-contrib'
+
+gem "rackup", "~> 2.2"
+gem "puma", "~> 6.6"
--- app.rb
+++ app.rb
@@ -15,14 +15,19 @@ end
 class Task < Sequel::Model
 end
 
+configure :development do
+  set :show_exceptions, true
+end
+
+
 get '/tasks' do
-  task = Task.all
+  task = Task.all.map(&:values)
   json task
 end
 
 post '/tasks' do
   data = JSON.parse(request.body.read)
-  task = Task.create(title: data['title'], description: data['description'])
+  task = Task.create(title: data.fetch('title'), description: data.fetch('description'))
   json task
 end
 
